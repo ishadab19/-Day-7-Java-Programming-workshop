da60f662ddc272ade6188a0f415510aa6a1f11d2
shadab <me.shadab19@gmail.com>
2022-10-12 19:34:17 +0530

    [Added- creating board]

--- workshopDay-7/src/com/tictactoa/TicTacToe.java
+++ workshopDay-7/src/com/tictactoa/TicTacToe.java
@@ -1,7 +1,17 @@
 package com.tictactoa;
 
 public class TicTacToe {
+
+	private static char[] board = new char[10];
+	
+	static void startGame() {
+		for (int i = 1; i <= 9; i++) {
+			board[i] = 1;
+			System.out.println(board);
+		}
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to tic tac toe");
+		System.out.println("Welcome to Tic Tac Toe ");
+		startGame();
 	}
 }
